d22ecb256facece5a329a6990b5df3199ce0038e
mayuri <mayurimhjn123@gmail.com>
2023-10-03 22:42:08 +0530

    my first commit

---
+++ src/sarthakprojectpackage/gitclass.java
@@ -0,0 +1,14 @@
+package sarthakprojectpackage;
+
+public class gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println("my name is sarthak");
+
+	}
+
+}
